b101daa27dadc724a4d9d91c846cb257b8121069
Ho3pLi <daniele.barile0101@gmail.com>
2023-09-21 11:49:42 +0200

    format JSON code quality improve

--- tools/format_JSON/formatJSON.js
+++ tools/format_JSON/formatJSON.js
@@ -10,38 +10,21 @@ var setting_mode;
 document.addEventListener('DOMContentLoaded', function(){
     if (document.title == 'Developer tools popup') {
         ffrawjson = document.getElementById('ffrawjson');
-        ffrawjson.addEventListener('click', function(){
+        ffrawjson.addEventListener('click', function(e){
+            e.stopImmediatePropagation();
             ffjson();
         });
     }else if(document.title == 'Format JSON'){
-        chrome.storage.local.get(null).then((res) => {
-            if (res != null) { //FIXME - non aggiorna la variabile e di conseguenza si rompe, forse non va bene il null?
-                thereIsSettings = true;
-            }else{
-                thereIsSettings = false;
-            }
-            console.log('æ thereIsSettings: ',thereIsSettings);
-            console.log('æ settings: ',res);
-        }).finally(() => {
-            if(thereIsSettings == false) {
-                chrome.storage.local.set({ CodeMirror_Setting_LineNumbers : true}).then(() => {
-                    setting_lineNumbers = true;
-                    console.log("æ lineNumbers is set.");
-                });
-                chrome.storage.local.set({ CodeMirror_Setting_Mode : 'javascript' }).then(() => {
-                    setting_mode = 'javascript';
-                    console.log("æ mode is set.");
-                }).finally(() => {
-                    chrome.storage.local.get(null).then((res) => {
-                        console.log('æ current cache: ',res);
-                    });
-                });
-            }else{
-                chrome.storage.local.get(null).then((res) => {
-                    console.log('æ current cache: ',res);
-                });
-            }
-        });
+        try {
+            chrome.storage.local.get(null, function(res) {
+                if (chrome.runtime.lastError) {
+                    console.error(chrome.runtime.lastError);
+                }
+                settingsHandler(res);
+            });
+        } catch (error) {
+            console.error(error);
+        }
         editor = document.getElementById('editor');
         rawJson = document.getElementById('inputjson');
         codeMirrorEditor = CodeMirror.fromTextArea(editor, {
@@ -62,12 +45,9 @@ async function ffjson() {
 
         const obj = JSON.parse(rawContent);
         console.log('æ obj: ', obj);
-        try {
-            navigator.clipboard.writeText(JSON.stringify(obj, null, 4));
-            console.log('æ successfully copied into the clipboard');
-        } catch (error) {
-            console.error(error);
-        }
+        navigator.clipboard.writeText(JSON.stringify(obj, null, 4));
+        console.log('æ successfully copied into the clipboard');
+        
     } catch (error) {
         console.error(error);
         alert('Check if the last content in your clipboard is a JSON!');
@@ -86,5 +66,38 @@ function fjson(){
         }
     } catch (error) {
         console.error(error);
+    }
+}
+
+function settingsHandler(res) {
+    var keys = Object.keys(res);
+    if (keys.length === 0) {
+        thereIsSettings = false;
+    }else{
+        thereIsSettings = true;
+    }
+    console.log('æ thereIsSettings: ',thereIsSettings);
+    console.log('æ settings: ',res);
+    initSettings();
+}
+
+function initSettings(){
+    if(thereIsSettings == false) {
+        chrome.storage.local.set({ CodeMirror_Setting_LineNumbers : true}).then(() => {
+            setting_lineNumbers = true;
+            console.log("æ lineNumbers is set.");
+        });
+        chrome.storage.local.set({ CodeMirror_Setting_Mode : 'javascript' }).then(() => {
+            setting_mode = 'javascript';
+            console.log("æ mode is set.");
+        }).finally(() => {
+            chrome.storage.local.get(null).then((res) => {
+                console.log('æ current cache: ',res);
+            });
+        });
+    }else{
+        chrome.storage.local.get(null).then((res) => {
+            console.log('æ current cache: ',res);
+        });
     }
 }
